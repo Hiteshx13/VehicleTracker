bd7e853ea2e5b93642eaa6378943ab11890e1103
Hitesh <imhr1727@gmail.com>
2021-11-29 16:52:33 +0200

    fragments added using nav graph

---
+++ app/src/main/java/com/scope/vehicletracker/owner/FragmentOwnerList.kt
@@ -0,0 +1,6 @@
+package com.scope.vehicletracker.owner
+
+import androidx.fragment.app.Fragment
+import com.scope.vehicletracker.R
+
+class FragmentOwnerList : Fragment(R.layout.fragment_owner_list)
---
+++ app/src/main/java/com/scope/vehicletracker/vehicle/FragmentMapUserVehicle.kt
@@ -0,0 +1,6 @@
+package com.scope.vehicletracker.vehicle
+
+import androidx.fragment.app.Fragment
+import com.scope.vehicletracker.R
+
+class FragmentMapUserVehicle : Fragment(R.layout.fragment_map_user_vehicle)
